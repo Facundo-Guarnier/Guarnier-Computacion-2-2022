c838addb61a4fe8a3149bc54b1cd97e2616606e8
Facundo Guarnier <fj.guarnier@alumno.um.edu.ar>
2022-04-19 10:42:38 -0300

    Mejoras vistas en clase

--- 5 - Inversor de caracteres (ej 6)/inversor.py
+++ 5 - Inversor de caracteres (ej 6)/inversor.py
@@ -33,45 +33,45 @@
 import argparse, os, sys
 
 
+def hijo(i, texto, pw):
+    fila = texto[i] 
+    invertida = ""
+    for a in range(len(fila)):
+        invertida += fila[-1-a]
+
+    with os.fdopen(pw, 'w') as w:
+        w.write(invertida)
+
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-f", type=str, required=True, help="Ruta del archivo.") 
 
     args = parser.parse_args()
 
-    with open(args.f, 'r') as file:
-        texto = file.readlines()
+    with open(args.f, 'r') as archivo:  #Se listan las filas del archivo
+        texto = archivo.readlines()
         cant_filas = len(texto)
 
-    for i in range(cant_filas):
-        r,w = os.pipe()
-        
+    pr,pw = os.pipe()                   #Creacion de la tuberia. 
+
+    for i in range(cant_filas):         #Tantos hijo como filas en el archivo.
         id_hijo = os.fork()
 
         if id_hijo == 0:
-            os.close(r)
-            w = os.fdopen(w, 'w')
-            
-            fila = texto[i] 
-            invertida = ""
-            for a in range(len(fila)):
-                invertida += fila[-1-a]
+            os.close(pr)                #Se cierra la lectura para el hijo
+            hijo(i, texto, pw)
+            sys.exit(0)                 #Muere el hijo
 
-            w.write(invertida)
-            w.close()
-            sys.exit(0)             #Muere el hijo
-
-        else:
-            os.close(w)
-            r = os.fdopen(r)
-            print(r.read(), end="")
-    r.close()
-    print("")
+    os.close(pw)
 
     for i in range(cant_filas):
-        os.wait()    
+        os.wait()
+    
+    with os.fdopen(pr) as r:
+        print(r.read())
 
-
+    sys.exit(0)
 
 if __name__=='__main__':
     main()
