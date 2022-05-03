ad37662b971d1640faff2f1bc7b35c528fb13998
root <root@Lenovo-V15-IIL.localdomain>
2022-05-03 14:21:35 -0300

    En clase

---
+++ 7-Rot13_con_multiprocessing-ej8/ej_8.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""
+    Escribir un programa que genere dos hijos utilizando multiprocessing.
+
+    Uno de los hijos deberá leer desde stdin texto introducido por el 
+    usuario, y deberá escribirlo en un pipe (multiprocessing).
+
+    El segundo hijo deberá leer desde el pipe el contenido de texto, lo 
+    encriptará utilizando el algoritmo ROT13, y lo almacenará en una cola 
+    de mensajes (multiprocessing).
+
+    El primer hijo deberá leer desde dicha cola de mensajes y mostrar el 
+    contenido cifrado por pantalla.
+"""
+import sys, multiprocessing
+
+from cupshelpers import Printer
+
+def leer(e,n):
+    sys.stdin = open(0)
+    for i in range(n):                  
+        e.send(sys.stdin.readline())
+    e.close()
+
+
+def encriptacion(l,n):
+    rot13 = str.maketrans(
+        'ABCDEFGHIJKLMabcdefghijklmNOPQRSTUVWXYZnopqrstuvwxyz',
+        'NOPQRSTUVWXYZnopqrstuvwxyzABCDEFGHIJKLMabcdefghijklm',
+        )
+    for i in range(n):
+        texto = l.recv().translate(rot13)
+        print(texto)
+
+if __name__ == "__main__":
+    p_lectura, p_escritura = multiprocessing.Pipe()
+
+    repeticion = 2      #No sabía cuantas lineas habia que leer jeje
+
+    h1 = multiprocessing.Process(target=leer, args=(p_escritura,repeticion))
+    h2 = multiprocessing.Process(target=encriptacion, args=(p_lectura,repeticion))
+    h1.start()
+    h2.start()
+    h1.join()
+    h2.join()
